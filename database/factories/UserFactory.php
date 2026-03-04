3eedc26049491d3f0790be8d6b58795d5022f5a5
Guilherme jeske <guilhermeieski@gmail.com>
2026-03-04 20:30:36 -0300

    só pra passar

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => 'Tester ' . Str::random(5),
+            'email' => 'tester+' . Str::random(5) . '@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
