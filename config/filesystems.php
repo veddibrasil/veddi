7eb9ccd3200ef531171ed1d023a9361ea39fec63
Guilherme jeske <guilhermeieski@gmail.com>
2026-03-25 18:35:42 -0300

    up for new repository

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,7 +56,7 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'visibility' => 'public',
+            // 'visibility' => 'public',
             'throw' => true,
             'report' => true,
         ],
